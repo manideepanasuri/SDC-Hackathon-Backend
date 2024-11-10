09c29062010993e5c8467626adc91bdaf1df9d93
manideepanasuri <139889116+manideepanasuri@users.noreply.github.com>
2024-11-10 16:51:26 +0530

    Update Passport.js

--- Routes/Passport.js
+++ Routes/Passport.js
@@ -9,7 +9,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: process.env.HOST+"/auth/google/callback",
     passReqToCallback: true,
   },
   async (request, accessToken, refreshToken, profile, done) => {
